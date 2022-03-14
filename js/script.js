afb46cc9a1c2a571a5ddc273a7552d61c20b36b0
Oleg <kekcoe@gmail.com>
2022-03-14 17:28:27 +0300

    Fixed some error

--- js/script.js
+++ js/script.js
@@ -17,22 +17,34 @@ const personalAnimeDB= {
     console.log('Вы киноман');
   }
   
-let iter= 0;
+let iter = 0;
 
 while(iter < 2){
 let a = prompt('Одно из последних просмотренных анимэ',"");
-  if(a.length < 3){
+  if(a == null || a.length < 3 ){
     do {
-      alert("Введи название длиннее");
+      alert("Введи название длиннее и не вздумай нажимать отмена");
     a = prompt('Одно из последних просмотренных анимэ',"");
-    }while (a.length <1);
+    }while (a.length < 3 || a == null );
   } else if (a.length > 50){    
     do {
       alert("Введи название короче");
       a = prompt('Одно из последних просмотренных анимэ',"");
   }while (a.length >50);
   }
-const b =  +prompt('Насколько оцените его', "");
+
+let b =  +prompt('Насколько оцените его', "");
+if( b == 0 || b.length < 1 ){
+  do {
+    alert("Введи значение и не вздумай нажимать отмена");
+    b =  +prompt('Насколько оцените его', "");
+  }while (b == 0 || b.length < 1 );
+} else if (b.length > 50){    
+  do {
+    alert("Введи название короче");
+    b =  +prompt('Насколько оцените его', "");
+}while (b == 0);
+}
 personalAnimeDB.anime[a] = b;
 iter++;
 }
